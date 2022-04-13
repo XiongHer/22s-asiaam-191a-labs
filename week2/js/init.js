3b5163581ebfa827510999ca32c6dd8b32c39d4f
XiongHer <xher21@g.ucla.edu>
2022-04-13 15:52:27 -0700

    update

--- week2/js/init.js
+++ week2/js/init.js
@@ -27,8 +27,8 @@ add_marker(37.620720,-105.745941, "Great Sand Dunes National Park <br> Visited i
 add_marker(35.965000,-111.973793, "Grand Canyon National Park<br> Visited in 2021 <img src='./photos/7.jpeg' class='myimages'></img>")
 add_marker(38.889271,-77.050179, "Lincoln Memorial<br> Visited in 2017,18, & 20 <img src='./photos/alm.jpg' class='myimages'></img>")
 add_marker(41.213181,-124.004623, "Redwood National & State Parks<br> Visited in 2021 <img src='./photos/redwood.jpeg' class='myimages'></img>")
-add_marker(44.427963, -110.588455, "Yellow Stone National Park")
-add_marker(43.714940, -102.548650, "Badland National Park")
+add_marker(44.427963, -110.588455, "Yellowstone National Park<br>Visited in 2016 <img src='./photos/ysp.jpg' class='myimages'></img>")
+add_marker(43.714940, -102.548650, "Badland National Park<br>Visited in 2016 <img src='./photos/blp.jpg' class='myimages'></img>")
 
 // var imageUrl ='https://www.doi.gov/sites/doi.gov/files/uploads/great-sand-dunes-np-nps-patrick-meyers-elk-and-star-dunes_0.jpg', 
 // imageBounds = [[37.620720,-105.745941],[37.620720,-105.745941]]
