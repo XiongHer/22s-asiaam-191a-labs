74520efbd8434a3dad22d5edef2959e76d9dac4a
XiongHer <xher21@g.ucla.edu>
2022-04-13 14:00:38 -0700

    update

--- week2/js/init.js
+++ week2/js/init.js
@@ -10,7 +10,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 //JavaScript let variable declaration to create a marker
 let marker = L.circleMarker([44.427963, -110.588455]).addTo(map) 
-        .bindPopup('Yellow Stone National Park<br> Visited in 2016 ')
+        .bindPopup('Yellow Stone National Park<br> Visited in 2016')
         .openPopup();
 let marker2 = L.circleMarker([43.714940, -102.548650]).addTo(map) 
         .bindPopup('Badland National Park<br> Visited in 2016 ')
@@ -23,10 +23,10 @@ function add_marker(lat,lng,popup){
         //.openPopup();
 }
 
-add_marker(37.620720,-105.745941, "Great Sand Dunes National Park <img src='./photos/greatsanddunes.jpeg' class='myimages'></img>")
-add_marker(35.965000,-111.973793, "Grand Canyon National Park<br> Visited in 2021")
-add_marker(38.889271,-77.050179, "Lincoln Memorial<br> Visited in 2017,18, & 20)")
-add_marker(41.213181,-124.004623, "Redwood National & State Parks<br> Visited in 2021")
+add_marker(37.620720,-105.745941, "Great Sand Dunes National Park <br> Visited in 2016 <img src='./photos/greatsanddunes.jpeg' class='myimages'></img>")
+add_marker(35.965000,-111.973793, "Grand Canyon National Park<br> Visited in 2021 <img src='./photos/7.jpeg' class='myimages'></img>")
+add_marker(38.889271,-77.050179, "Lincoln Memorial<br> Visited in 2017,18, & 20 <img src='./photos/alm.jpg' class='myimages'></img>")
+add_marker(41.213181,-124.004623, "Redwood National & State Parks<br> Visited in 2021 <img src='./photos/redwood.jpeg' class='myimages'></img>")
 
 // var imageUrl ='https://www.doi.gov/sites/doi.gov/files/uploads/great-sand-dunes-np-nps-patrick-meyers-elk-and-star-dunes_0.jpg', 
 // imageBounds = [[37.620720,-105.745941],[37.620720,-105.745941]]
