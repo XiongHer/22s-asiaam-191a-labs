5b86b757da6f92d32613384a86ef670cdef34fdd
XiongHer <xher21@g.ucla.edu>
2022-05-10 20:17:23 -0700

    update

--- week6/js/init.js
+++ week6/js/init.js
@@ -10,20 +10,20 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 // create a function to add markers
 function addMarker(data){
-    console.log(data)
+    // console.log(data)
     L.marker([data.lat,data.lng]).addTo(map).bindPopup(`<h2>${data['How was your graduate experiences so far at UCLA?']}</h2> <h3>${data['How and where do you find academic and social support?']}</h3>`)
-    createButtons(data.lat,data.lng,data['Location'])
+    createButtons(data.lat,data.lng,data['How was your graduate experiences so far at UCLA?'])
     return 
 }
 
-function createButtons(data){
+function createButtons(lat,lng,title){
     const newButton = document.createElement("button"); // adds a new button
-    newButton.id = "button"+data.Location; // gives the button a unique id
-    newButton.innerHTML = data.Location; // gives the button a title
-    newButton.setAttribute("lat",data.lat); // sets the latitude 
-    newButton.setAttribute("lng",data.lng); // sets the longitude 
+    newButton.id = "button"+title; // gives the button a unique id
+    newButton.innerHTML = title; // gives the button a title
+    newButton.setAttribute("lat",lat); // sets the latitude 
+    newButton.setAttribute("lng",lng); // sets the longitude 
     newButton.addEventListener('click', function(){
-        map.flyTo([data.lat,data.lng]); //this is the flyTo from Leaflet
+        map.flyTo([lat,lng]); //this is the flyTo from Leaflet
     })
     const placeForButtons = document.getElementById('placeForButtons')
     spaceForButtons.appendChild(newButton);//this adds the button to our page.
@@ -43,7 +43,7 @@ function processData(results){
     console.log(results)
     results.data.forEach(data => {
         console.log(data)
-        addMarker(data.lat,data.lng,data['Location'])
+        addMarker(data)
     })
 }
 
